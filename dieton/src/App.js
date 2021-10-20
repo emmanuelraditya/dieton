73798b78d3d7fc5a68c28e106fc04ea69d1fa972
ODerryO <derrygera@gmail.com>
2021-10-20 11:11:25 +0800

    home user update

--- dieton/src/App.js
+++ dieton/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Routes from './routes';
-import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Routes />
     </div>
   );
